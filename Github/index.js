7c2f964918b88cc88f4b352ac5dce9bea30cc496
Syed Muhammad Usman <syedmuhammadusman132@gmail.com>
2024-12-17 11:00:51 +0500

    Add files via upload

---
+++ Github/index.js
@@ -0,0 +1,48 @@
+let allQuestion=[
+    {
+        question:"CPU stands for?",
+        options:[
+            {answer:" Central Processing Unit ",iscorrect:true},
+            {answer:" arithmatic logic unit ",iscorrect:false},
+            {answer:" control unit  ",iscorrect:false},
+            {answer:" memory unit",iscorrect:false}
+        ]
+    },
+    {
+        question:"JS stands for?",
+        options:[
+            {answer:" Javascript ",iscorrect:true},
+            {answer:" Hyper text markup Language ",iscorrect:false},
+            {answer:" Cascading Style Sheet ",iscorrect:false},
+            {answer:" Bootstrap ",iscorrect:false}
+        ]
+    },
+    {
+        question:"Html stands for?",
+        options:[
+            {answer:" Hyper text markup Language ",iscorrect:true},
+            {answer:" Hyper text markup Language ",iscorrect:false},
+            {answer:" Cascading Style Sheet ",iscorrect:false},
+            {answer:" Bootstrap ",iscorrect:false}
+        ]
+    },
+    {
+        question:"JS stands for?",
+        options:[
+            {answer:" Javascript ",iscorrect:true},
+            {answer:" Hyper text markup Language ",iscorrect:false},
+            {answer:" Cascading Style Sheet ",iscorrect:false},
+            {answer:" Bootstrap ",iscorrect:false}
+        ]
+    },
+    {
+        question:"JS stands for?",
+        options:[
+            {answer:" Javascript ",iscorrect:true},
+            {answer:" Hyper text markup Language ",iscorrect:false},
+            {answer:" Cascading Style Sheet ",iscorrect:false},
+            {answer:" Bootstrap ",iscorrect:false}
+        ]
+    },
+]
+console.log(allQuestion);
